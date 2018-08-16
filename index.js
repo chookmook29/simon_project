25176ad95536a0c038b0010497cad2cb071d1251
Piotr Chudziak <strangelove44@gmail.com>
2018-08-16 09:53:42 +0100

    Button sounds added, index.js created

---
+++ index.js
@@ -0,0 +1,42 @@
+function push_red(){
+       var audio = new Audio();
+	   audio.src = "sounds/button_1.mp3";
+	   audio.preload = 'auto';
+       audio.play();
+                 }
+function push_blue(){
+       var audio = new Audio();
+	   audio.src = "sounds/button_1.mp3";
+	   audio.preload = 'auto';
+       audio.play();
+                 }
+function push_green(){
+       var audio = new Audio();
+	   audio.src = "sounds/button_1.mp3";
+	   audio.preload = 'auto';
+       audio.play();
+                 }
+function push_orange(){
+       var audio = new Audio();
+	   audio.src = "sounds/button_1.mp3";
+	   audio.preload = 'auto';
+       audio.play();
+                 }
+function push_start(){
+       var audio = new Audio();
+	   audio.src = "sounds/button_2.mp3";
+	   audio.preload = 'auto';
+       audio.play();
+                 }
+function push_strict(){
+       var audio = new Audio();
+	   audio.src = "sounds/button_2.mp3";
+	   audio.preload = 'auto';
+       audio.play();
+                 }
+function push_reset(){
+       var audio = new Audio();
+	   audio.src = "sounds/button_2.mp3";
+	   audio.preload = 'auto';
+       audio.play();
+                 }
