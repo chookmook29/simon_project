e180dd8c9882c7d184e94405c61cefe25a623467
Piotr Chudziak <strangelove44@gmail.com>
2018-10-13 13:09:27 +0100

    Refactor of display_clear() function

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ var other_count = 0;
 var yet_another_count = 1;
 const first_interval = 1000;
 const second_interval = 1050;
+var colors = ["red", "blue", "green", "orange"];
 
 function push_power_on(){
      var audio = new Audio();
@@ -60,39 +61,37 @@ function display_array() {
   colour = document.getElementById(game_array[count]);
   colour.style.backgroundColor=game_array[count];
   if(game_array[count] === "red"){
-    colour.style.boxShadow="0 0 0 1px red inset, 0 0 0 2px red inset, 0 7px 0 0 red, 0 8px 0 1px #020202, 0 8px 8px 1px #070707";
+    colour.style.boxShadow = "0 0 0 1px red inset, 0 0 0 2px red inset, 0 7px 0 0 red, 0 8px 0 1px #020202, 0 8px 8px 1px #070707";
   }
   else if(game_array[count] === "blue"){
     colour.style.boxShadow="0 0 0 1px blue inset, 0 0 0 2px blue inset, 0 7px 0 0 blue, 0 8px 0 1px #020202, 0 8px 8px 1px #070707";
   }
   else if(game_array[count] === "green"){
-    colour.style.boxShadow="0 0 0 1px green inset, 0 0 0 2px green inset, 0 7px 0 0 green, 0 8px 0 1px #020202, 0 8px 8px 1px #070707";
+    colour.style.boxShadow = "0 0 0 1px green inset, 0 0 0 2px green inset, 0 7px 0 0 green, 0 8px 0 1px #020202, 0 8px 8px 1px #070707";
   }
   else{
-    colour.style.boxShadow="0 0 0 1px orange inset, 0 0 0 2px orange inset, 0 7px 0 0 orange, 0 8px 0 1px #020202, 0 8px 8px 1px #070707";
+    colour.style.boxShadow =" 0 0 0 1px orange inset, 0 0 0 2px orange inset, 0 7px 0 0 orange, 0 8px 0 1px #020202, 0 8px 8px 1px #070707";
   }
   count++;
   if (count == game_array.length){
     clearInterval(var2);
   }
 }
+
+function set_background(color) {
+  var element = document.getElementById(color);
+  element.style.backgroundColor = "gray";
+  element.style.boxShadow = "0 0 0 1px #8E8E8E inset, 0 0 0 2px #777777 inset, 0 7px 0 0 #727272, 0 8px 0 1px #020202, 0 8px 8px 1px #070707";
+}
 function display_clear(){
-  red = document.getElementById("red");
-	red.style.backgroundColor="gray";
-	red.style.boxShadow="0 0 0 1px #8E8E8E inset, 0 0 0 2px #777777 inset, 0 7px 0 0 #727272, 0 8px 0 1px #020202, 0 8px 8px 1px #070707";
-	blue = document.getElementById("blue");
-	blue.style.backgroundColor="gray";
-	blue.style.boxShadow="0 0 0 1px #8E8E8E inset, 0 0 0 2px #777777 inset, 0 7px 0 0 #727272, 0 8px 0 1px #020202, 0 8px 8px 1px #070707";
-	green = document.getElementById("green");
-	green.style.backgroundColor="gray";
-	green.style.boxShadow="0 0 0 1px #8E8E8E inset, 0 0 0 2px #777777 inset, 0 7px 0 0 #727272, 0 8px 0 1px #020202, 0 8px 8px 1px #070707";
-	orange = document.getElementById("orange");
-	orange.style.backgroundColor="gray";
-	orange.style.boxShadow="0 0 0 1px #8E8E8E inset, 0 0 0 2px #777777 inset, 0 7px 0 0 #727272, 0 8px 0 1px #020202, 0 8px 8px 1px #070707";
+  colors.forEach(color => {
+  set_background(color);
+  });
   if (count == game_array.length){
     clearInterval(var3);
   }
 }
+
 
 function push_first(){
 	current_button = "red";
@@ -196,21 +195,6 @@ function push_fourth(){
       document.getElementById("screen").innerHTML = level;}
 		add_array_element();
   }
-}
-
-function default_colour(){
-	red = document.getElementById("red");
-	red.style.backgroundColor="gray";
-	red.style.boxShadow="0 0 0 1px #8E8E8E inset, 0 0 0 2px #777777 inset, 0 7px 0 0 #727272, 0 8px 0 1px #020202, 0 8px 8px 1px #070707";
-	blue = document.getElementById("blue");
-	blue.style.backgroundColor="gray";
-	blue.style.boxShadow="0 0 0 1px #8E8E8E inset, 0 0 0 2px #777777 inset, 0 7px 0 0 #727272, 0 8px 0 1px #020202, 0 8px 8px 1px #070707";
-	green = document.getElementById("green");
-	green.style.backgroundColor="gray";
-	green.style.boxShadow="0 0 0 1px #8E8E8E inset, 0 0 0 2px #777777 inset, 0 7px 0 0 #727272, 0 8px 0 1px #020202, 0 8px 8px 1px #070707";
-	orange = document.getElementById("orange");
-	orange.style.backgroundColor="gray";
-	orange.style.boxShadow="0 0 0 1px #8E8E8E inset, 0 0 0 2px #777777 inset, 0 7px 0 0 #727272, 0 8px 0 1px #020202, 0 8px 8px 1px #070707";
 }
 
 function add_array_element(){
